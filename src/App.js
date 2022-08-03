ed9e1973c460d6a17537cfefb2356fadadf1bd17
rudrabhikadiya3 <rudra.rnw@gmail.com>
2022-08-03 15:20:11 +0530

    persister

--- src/App.js
+++ src/App.js
@@ -1,26 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
-import { Route, Switch } from 'react-router-dom';
-import Medicine from './container/Medicine';
-import Patients from './container/Patients';
-import Layout from './components/Layout';
-import { Provider } from 'react-redux';
-import configureStore from './redux/store';
-import Counter from './container/Counter';
+import { Route, Switch } from "react-router-dom";
+import Medicine from "./container/Medicine";
+import Patients from "./container/Patients";
+import Layout from "./components/Layout";
+import { Provider } from "react-redux";
+import configureStore from "./redux/store";
+import Counter from "./container/Counter";
+
+import { PersistGate } from "redux-persist/integration/react";
 
 function App() {
-  const store = configureStore()
+  const { store, persistor} = configureStore();
   return (
-    <>    
-    <Provider store={store}>
-      <Layout>
-        <Switch>
-          <Route path={'/medicine'} exact component={Medicine}/>
-          <Route path={'/patients'} exact component={Patients}/>
-          <Route path={'/counter'} exact component={Counter}/>
-        </Switch>
-      </Layout>
+    <>
+      <Provider store={store}>
+        <PersistGate loading={null} persistor={persistor}>
+          <Layout>
+            <Switch>
+              <Route path={"/medicine"} exact component={Medicine} />
+              <Route path={"/patients"} exact component={Patients} />
+              <Route path={"/counter"} exact component={Counter} />
+            </Switch>
+          </Layout>
+        </PersistGate>
       </Provider>
     </>
   );
--- src/redux/store.js
+++ src/redux/store.js
@@ -2,10 +2,19 @@ import { applyMiddleware, createStore } from "redux";
 import thunk from "redux-thunk";
 import rootReducer from './index'
 
+import { persistStore, persistReducer } from 'redux-persist'
+import storage from 'redux-persist/lib/storage'
 
+const persistConfig = {
+    key: 'root',
+    storage,
+  }
+ 
+  const persistedReducer = persistReducer(persistConfig, rootReducer)
 const configureStore = () => {
-    const store = createStore(rootReducer, applyMiddleware(thunk));
-    return store
+    const store = createStore(persistedReducer, applyMiddleware(thunk));
+    let persistor = persistStore(store)
+    return { store, persistor }
 }
 
 export default configureStore;
