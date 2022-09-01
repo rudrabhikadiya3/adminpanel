4d3ccf37cce7b8e487b40351cbac9ce5f46c70bb
Rudra Bhikadiya <99029749+rudrabhikadiya3@users.noreply.github.com>
2022-09-01 10:58:07 +0530

    add whitelist in store

--- src/redux/store.js
+++ src/redux/store.js
@@ -8,6 +8,7 @@ import storage from 'redux-persist/lib/storage'
 const persistConfig = {
     key: 'root',
     storage,
+    whitelist : ['counter']
   }
  
   const persistedReducer = persistReducer(persistConfig, rootReducer)
